249ba6a07d2502bcb0c032f118807f45ff715976
Sachin Kumar <sachinbnp2211@gmail.com>
2026-06-11 07:17:14 +0530

    Time: 79 ms (11.36%) | Memory: 52.3 MB (5.71%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int>p=arr;
+        sort(p.begin(),p.end());
+        unordered_map<int,int>mp;
+        for(int val:arr)
+        {
+            mp[val]++;
+        }
+        priority_queue<int>pq;
+        for(auto it:mp)
+        {
+            pq.push(it.first);
+
+        }
+        unordered_map<int,int>r;
+        while(!pq.empty())
+        {
+          r[pq.top()]=pq.size();
+          pq.pop();  
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            arr[i]=r[arr[i]];
+        }
+        return arr;
+
+        
+
+        
+    }
+};
