56122a8eee2437ee160ff05cad82b1985ad3f67d
Sachin Kumar <sachinbnp2211@gmail.com>
2026-06-16 17:55:25 +0530

    Time: 1515 ms (70.61%) | Memory: 515.7 MB (10.70%) - LeetSync

--- 4322-frequency-balance-subarray/frequency-balance-subarray.cpp
+++ 4322-frequency-balance-subarray/frequency-balance-subarray.cpp
@@ -1,38 +1,52 @@
 class Solution {
 public:
-    int getLength(vector<int>& a) {
-        int n = a.size(), ans = 1;
-        for (int i = 0; i < n; i++) {
-            map<int, int> mp;      // frequency of each value
-            vector<int> fq(n + 1); // count of frequencies
-            int mx = 0; // maximum frequency
-            int s = 0;  // number of values having frequency mx
-            for (int j = i; j < n; j++) {
-                if (mp[a[j]])
-                    fq[mp[a[j]]]--; // remove old frequency
-                mp[a[j]]++;
-                fq[mp[a[j]]]++; // add new frequency
-                if (mp[a[j]] > mx) {
-                    mx = mp[a[j]];
-                    s = 1; // new maximum frequency found
+    int getLength(vector<int>& nums) {
+        int ans=1;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            unordered_map<int,int>freq;
+            map<int,int>ff;
+            for(int k=i;k<n;k++)
+            { int j=nums[k];
+                if(freq[j]>0)
+                {
+                    ff[freq[j]]--;
+                
+
+                
+                if(ff[freq[j]]==0)
+                {
+                
+                    ff.erase(freq[j]);
                 }
-                else if (mp[a[j]] == mx) {
-                    s++; // another value reaches maximum frequency
                 }
-                int distinct = mp.size();
-                // only one distinct value
-                if (distinct == 1) {
-                    ans = max(ans, j - i + 1);
+                
+                
+               
+                ff[++freq[j]]++;
+                if(freq.size()==1)
+                {
+                    ans=max(ans,k-i+1);
+                    continue;
                 }
-                // max frequency must be twice every other frequency
-                else if (mx % 2 == 0 &&
-                         s < distinct &&
-                         fq[mx / 2] == distinct - s) {
-                    ans = max(ans, j - i + 1);
+                if(ff.size()==2)
+                { auto it=ff.begin();
+                    
+                    int f1=it->first;
+                    ++it;
+                    int f2=it->first;
+                    if(f2==2*f1)
+                    {
+                        ans=max(ans,k-i+1);
+
+                    }
                 }
+                
+
             }
         }
-
         return ans;
+        
     }
 };
